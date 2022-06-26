8d3d2abeea9bfa744b512a55ca16c37fb2038304
rmbell44 <107881804+rmbell44@users.noreply.github.com>
2022-06-26 10:52:18 -0400

    Add files via upload
    Python code to upload a csv file and save as new updated file

---
+++ fileImport.py
@@ -0,0 +1,33 @@
+import csv
+
+import pandas as pd
+from pymongo import MongoClient
+
+connection_string = "mongodb://127.0.0.1:27017/cve"
+client = MongoClient(connection_string)
+#connection string to MongoDBB
+
+db = client.cvedata
+#database to use 'cvedata'
+mydb = client["cvedata"]
+mycol= mydb["2018-2"]
+#collection created '2018-2'
+
+
+file = pd.read_csv(r"/Users/rachelbell/Desktop/2018.csv")
+#reader csv file from desktop
+
+file = file.drop(file.columns[[0, 2, 3,5,6,8,9,10,11,12,13,14]], axis=1)
+#remove unwanted columns (CWE ID, # of Exploits, etc.
+
+file.columns = ['CVE ID', 'Vulnerability Type(s)', 'Score']
+#header names
+
+file = file.sample(n=5000)
+#accessing sample of 5000 entries
+
+file.to_csv("2018_updated.csv", index=False)
+#updated file name
+
+
+
